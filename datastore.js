12d4fff73a6a53a6365f85e63c7540fa6b4ea20a
mgwedd <michael.wedd@gmail.com>
2019-03-06 13:48:22 -0500

    stubbed out state management functions and changed DB to object-based

--- datastore.js
+++ datastore.js
@@ -1,18 +1,30 @@
-const STORE = [
-    {
-        question: 'What is Google Cloud Platform (GCP)?',
-        wrong: ['A collection of products that includes Youtube, Gmail and Search, which Google offers to customers on a freemium basis, with the overarching purpose of organizing the world’s information',
-                        'A service that Google offers to enterprises for developing, launching, and deploying their own managed web applications', 
-                        'The world’s number one cloud computing platform'],
-        right: 'A suite of enterprise cloud computing services that runs on the same infrastructure that Google uses internally for its end-user products'
+// STORE is our database.
+// It's an object literal with two properties: 'progress' and 'questions'. 
+// The value of 'progress' is an object with two key/val pairs: the current question and the score.
+// The value of 'questions' is an array containing objects, which have three props: 'question', 'wrong' with an array of wrong strings, and 'right' with the correct answer string 
 
-    }, 
-    {
-        question: 'What Is GCP’s Compute Engine Useful for?',
-        wrong: ['Applying a lockstep columnar traversal algorithm to massive datasets',
-                'Ingesting and processing streaming data from Internet of Things devices', 
-                'Managing noSQL datastores'],
-        right: 'Launching, provisioning and decommissioning virtual machines (VMs) on demand'
-
-    }
-]
+const STORE = {
+    progress: {
+        questionNum: 0, 
+        score: 0
+    },
+    questions: [
+        
+        {
+            question: 'What is Google Cloud Platform (GCP)?',
+            wrong: ['A collection of products that includes Youtube, Gmail and Search, which Google offers to customers on a freemium basis, with the overarching purpose of organizing the world’s information',
+                            'A service that Google offers to enterprises for developing, launching, and deploying their own managed web applications', 
+                            'The world’s number one cloud computing platform'],
+            right: 'A suite of enterprise cloud computing services that runs on the same infrastructure that Google uses internally for its end-user products'
+    
+        }, 
+        {
+            question: 'What Is GCP’s Compute Engine Useful for?',
+            wrong: ['Applying a lockstep columnar traversal algorithm to massive datasets',
+                    'Ingesting and processing streaming data from Internet of Things devices', 
+                    'Managing noSQL datastores'],
+            right: 'Launching, provisioning and decommissioning virtual machines (VMs) on demand'
+    
+        }
+    ]
+}
--- state-manager.js
+++ state-manager.js
@@ -1,9 +1,62 @@
-// three main render functions: 
+function renderFront() {
+    // renders home screen and allows user to start the quiz
 
-// renderFront
-// renderQuestionPage
-// (validation renders?)
-// renderEnd
+    // In case this is a quiz restart, reset the question number and score.
+    STORE.progress.score = 0;
+    STORE.progress.questionNum = 0;
 
-// Remember with jQuery, if you add event listeners, the element has to be in the dom first. Or 
-// use delegation
+    // $('main').on('click', '#start', function (event) {
+         //startQuiz();
+    // )};
+}
+
+function startQuiz() {
+    // if user hits start, begin quiz and render question.
+    
+    // need first to remove front page material like start button.
+
+    renderQuestion();
+}
+
+function renderQuestion() {
+    // renders a question page
+
+    // grab a new question
+    // update the questionNum and render it to the progress banner.
+    // populate form with answers
+    // change button to 'submit'
+    // call the validator to listen for submit and validate.
+}
+
+function validateResponse(){
+    // listen for submit
+    // on submit, validate answer. 
+    // if correct, 
+        // call correct screen renderer
+    // if incorrect, 
+        // call incorrect screen renderer
+}
+
+// PROBLEM: how to get to next question?
+
+function renderResponseCorrect() {
+    // render correct answer screen with data from validate.
+
+}
+
+function renderResponseIncorrect() {
+    // render correct answer screen with data from validate.
+    
+}
+
+function renderEnd() {
+ // if state tracker was index of Store is length -1, then render the end screen.
+ // must be able to call restart the quiz and clear state.
+}
+
+
+function createQuiz() {
+    renderFront();
+}
+
+$(createQuiz);
