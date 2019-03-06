6e8fd09994ff7509f923f5f715226645dec2dfee
mgwedd <michael.wedd@gmail.com>
2019-03-06 09:08:45 -0500

    added two entries to datastore and stubed out two functions in state-manager

---
+++ datastore.js
@@ -0,0 +1,18 @@
+const STORE = [
+    {
+        question: 'What is Google Cloud Platform (GCP)?',
+        wrong: ['A collection of products that includes Youtube, Gmail and Search, which Google offers to customers on a freemium basis, with the overarching purpose of organizing the world’s information',
+                        'A service that Google offers to enterprises for developing, launching, and deploying their own managed web applications', 
+                        'The world’s number one cloud computing platform'],
+        right: 'A suite of enterprise cloud computing services that runs on the same infrastructure that Google uses internally for its end-user products'
+
+    }, 
+    {
+        question: 'What Is GCP’s Compute Engine Useful for?',
+        wrong: ['Applying a lockstep columnar traversal algorithm to massive datasets',
+                'Ingesting and processing streaming data from Internet of Things devices', 
+                'Managing noSQL datastores'],
+        right: 'Launching, provisioning and decommissioning virtual machines (VMs) on demand'
+
+    }
+]
---
+++ state-manager.js
@@ -0,0 +1,9 @@
+// three main render functions: 
+
+// renderFront
+// renderQuestionPage
+// (validation renders?)
+// renderEnd
+
+// Remember with jQuery, if you add event listeners, the element has to be in the dom first. Or 
+// use delegation
